2f9d087ace8d5b5a8412289322dc5d20ed8df72c
David Enrique Palacios Garcia <paladavid@hotmail.com>
2021-04-14 14:43:21 -0500

    Favicon updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,34 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    let valor=2;
-    $('#rangeBar').on('input', function() {
+    let valor = 2;
+    $('#rangeBar').on('input', function () {
         valor = $("#rangeBar").val();
 
-        if(valor==0)
-        {
+        if (valor == 0) {
             $("#quantity").html("10K");
             $("#value").html("$8.00");
         }
-        else if(valor==1)
-        {
+        else if (valor == 1) {
             $("#quantity").html("50K");
             $("#value").html("$12.00");
         }
-        
-        else if(valor==2)
-        {
+
+        else if (valor == 2) {
             $("#quantity").html("100K");
             $("#value").html("$16.00");
         }
-        else if(valor==3)
-        {
+        else if (valor == 3) {
             $("#quantity").html("500K");
             $("#value").html("$24.00");
         }
-        else if(valor==4)
-        {
+        else if (valor == 4) {
             $("#quantity").html("1M");
             $("#value").html("$36.00");
         }
-        
-      });
 
-    
+    });
+
+
 
 });
